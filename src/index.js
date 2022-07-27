20bacd900d1825c3479302561a752013dcbc8e04
fukunokaze <fukunokaze@gmail.com>
2022-07-27 23:26:18 +0700

    [Update code]

--- src/index.js
+++ src/index.js
@@ -4,14 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {ApolloClient, HttpLink, InMemoryCache, ApolloProvider} from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:8080/",
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    fetch, 
+   }),
+  cache: new InMemoryCache()
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,41 @@
+import { gql, useMutation } from "@apollo/client";
 import React, { useState } from "react";
 import Label from "../components/Label";
 
 function Login(){
     const [message, setMessage] = useState("");
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    function usernameOnChange(e){
+        setUserName(e.target.value);
+    }
+
+    function passwordOnChange(e){
+        setPassword(e.target.value);
+    }
+    
+    // const query = gql`
+    //     mutation{
+    //         authenticate(input:{
+    //             username:"arga", password:"password"
+    //         }){id, name}
+    //     }`;
+
+    const AUTHENTICATE = gql`
+        mutation Authenticate ($username: String!, $password: String!){
+            authenticate(username: $username, password: $password){
+                id
+                name                
+            }
+        }`;
+    
+    const [authenticate] = useMutation(AUTHENTICATE);
+
+    function onSubmitHandler(e){
+        e.preventDefault();
+        authenticate({variables: {username: username, password: password}});
+    }
 
 return(
     <div id='logincontainer'>
@@ -14,36 +47,38 @@ return(
         <div className="column">
             <img src="../assets/Images/00_login_page_globe.jpg" alt="" />
         </div>    
+            
         <div className="column">
-<form>          Forgot your password? click here 
-            <div className="information">
-            {/* <%=MessageFactory.GetMessage("MFSSec0007", MessageType.Information).Value %>, 
-            <%=Html.ActionLink(MessageFactory.GetMessage("MFS0078", MessageType.Information).Value, "RequestChangePassword","ChangePassword")%>                     */}
-            <Label Text={message}/>
-            </div>
-            <p></p>
-            <table cellspacing="0.5px">
-                <tr>
-                    <td><Label Text="Username"/></td>
-                    <td><input id="username" type="text"></input></td>
-                </tr>
-                <tr>
-                <td><Label Text="Password"/></td>
-                <td><input id="password" type="password"></input></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="image" value="Login" src="../assets/Images/login.jpg" style={{width:71, height:21}} alt="login" /></td>
-                </tr>
-            </table>
+            <form onSubmit={onSubmitHandler}>  
+                Forgot your password? click here 
+                <div className="information">
+                {/* <%=MessageFactory.GetMessage("MFSSec0007", MessageType.Information).Value %>, 
+                <%=Html.ActionLink(MessageFactory.GetMessage("MFS0078", MessageType.Information).Value, "RequestChangePassword","ChangePassword")%>                     */}
+                <Label Text={message}/>
+                </div>
+                <p></p>
+                <table cellspacing="0.5px">
+                    <tr>
+                        <td><Label Text="Username"/></td>
+                        <td><input id="username" type="text" onChange={usernameOnChange} value={username}></input></td>
+                    </tr>
+                    <tr>
+                    <td><Label Text="Password"/></td>
+                    <td><input id="password" type="password" onChange={passwordOnChange} value={password}></input></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="image" value="Login" src="../assets/Images/login.jpg" style={{width:71, height:21}} alt="login" /></td>
+                    </tr>
+                </table>
             </form>  
         </div>
         <div className="footer clear">
